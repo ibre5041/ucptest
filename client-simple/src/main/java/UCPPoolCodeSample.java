376ed009a3e5ebb6c2d00a00bfb5ef1f1fb2ef98
ibre5041 <ibre5041@ibrezina.net>
2018-02-28 21:04:27 +0100

    trivial standalone code sample

---
+++ client-simple/src/main/java/UCPPoolCodeSample.java
@@ -0,0 +1,74 @@
+import java.io.File;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import oracle.ucp.jdbc.PoolDataSource;
+import oracle.ucp.jdbc.PoolDataSourceFactory;
+
+
+public class UCPPoolCodeSample {
+	
+	// UCP XML config file location URI
+	private static File ucpConfig = new File(UCPPoolCodeSample.class.getClassLoader().getResource("ucp.xml").getFile());
+	private static final String ucpConfigURI = ucpConfig.toURI().toString();
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Shared pool configuration using XML: " + ucpConfigURI);
+
+		// Java system property to specify the location of UCP XML configuration
+		// file which has shared pool, datasource properties defined in it.
+		System.setProperty("oracle.ucp.jdbc.xmlConfigFile", ucpConfigURI);
+
+		// Get the datasource instance named as "UCPPoolA1" in XML config file ucp.xml
+		PoolDataSource ds1 = PoolDataSourceFactory.getPoolDataSource("UCPPoolA1");
+
+		// Get a connection datasource
+		try(Connection conn1 = ds1.getConnection()) {
+
+			// Run a query on the connection
+			runQuery(conn1);
+
+			// return connection to the pool
+			conn1.close();
+		}
+
+		// Get the datasource
+		PoolDataSource ds2 = PoolDataSourceFactory.getPoolDataSource("UCPPoolA2");
+
+		// Get a connection using tenant2 datasource
+		try(Connection conn2 = ds2.getConnection()) {
+
+			// Run a query on the connection obtained using tenant2 datasource i.e.
+			// tenant2_ds
+			runQuery(conn2);
+
+			// return connection to the pool
+			conn2.close();
+		}
+
+	}
+
+	/**
+	 * Runs a query on the book table using the given connection.
+	 */
+	private static void runQuery(Connection conn) {
+		try {
+			String sql = "SELECT ID, NAME, AUTHOR FROM BOOK WHERE ROWNUM < 10";
+			try(Statement st = conn.createStatement()) {
+				try(ResultSet rs = st.executeQuery(sql)) {
+					System.out.println("Books Details:");
+					while (rs.next()) {
+						System.out.println("ID = " + rs.getLong("ID") + "     NAME = " + rs.getString("NAME"));
+					}
+					rs.close();
+				}
+				st.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ client-simple/src/main/java/UCPPoolCodeSampleMT.java
@@ -0,0 +1,91 @@
+import java.io.File;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import oracle.ucp.jdbc.PoolDataSource;
+import oracle.ucp.jdbc.PoolDataSourceFactory;
+
+public class UCPPoolCodeSampleMT {
+	
+	private static class RunnableThread implements Runnable {
+		private Thread t;
+		private String threadName;
+		private PoolDataSource pool;
+
+		RunnableThread( String name, PoolDataSource pool) {
+			this.threadName = name;
+			this.pool = pool;
+			System.out.println("Creating " +  threadName );
+		}
+
+		private static void runQuery(Connection conn) {
+			try {
+				String sql = "SELECT ID, NAME, AUTHOR FROM BOOK WHERE ROWNUM < 10";
+				try(Statement st = conn.createStatement()) {
+					try(ResultSet rs = st.executeQuery(sql)) {
+						System.out.println("Books Details:");
+						while (rs.next()) {
+							System.out.println("ID = " + rs.getLong("ID") + "     NAME = " + rs.getString("NAME"));
+						}
+						rs.close();
+					}
+					st.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+		public void run() {
+			System.out.println("Running " +  threadName );
+
+			// Get a connection from datasource
+			try(Connection conn = pool.getConnection()) {
+				for(int i = 4; i > 0; i--) {
+					// Run a query on the connection
+					runQuery(conn);
+					System.out.println("Thread: " + threadName + ", " + i);
+					// Let the thread sleep for a while.
+					Thread.sleep(50);					
+				}
+				
+				// return connection to the pool
+				conn.close();
+			} catch (InterruptedException | SQLException e) {
+				System.out.println("Thread " +  threadName + " interrupted.");
+			}
+			System.out.println("Thread " +  threadName + " exiting.");
+		}
+
+		public void start () {
+			System.out.println("Starting " +  threadName );
+			if (t == null) {
+				t = new Thread (this, threadName);
+				t.start ();
+			}
+		}
+	}
+
+	// UCP XML config file location URI
+	private static File ucpConfig = new File(UCPPoolCodeSample.class.getClassLoader().getResource("ucp.xml").getFile());
+	private static final String ucpConfigURI = ucpConfig.toURI().toString();
+
+	public static void main(String args[]) throws Exception {
+		// Java system property to specify the location of UCP XML configuration
+		// file which has shared pool, datasource properties defined in it.
+		System.setProperty("oracle.ucp.jdbc.xmlConfigFile", ucpConfigURI);
+		
+		// Get the datasource instance named as "UCPPoolA1" in XML config file ucp.xml
+		PoolDataSource ds = PoolDataSourceFactory.getPoolDataSource("UCPPoolA1");
+		
+		// Start N parallel working threads - where N is close to pool size
+		for (int i = 1; i < ds.getMaxPoolSize() - 5; i++)
+		{
+			RunnableThread R1 = new RunnableThread("Thread-"+i, ds);
+			R1.start();
+		}
+	}   
+}
