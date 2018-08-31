d352662049704f138a7862d5a23a6d8ef69e5869
ibre5041 <ibre5041@ibrezina.net>
2018-08-31 13:28:13 +0200

    standalone web REST client

--- standalone-web-client/src/main/java/sandbox/rest/BookClient.java
+++ standalone-web-client/src/main/java/sandbox/rest/BookClient.java
@@ -1,6 +1,10 @@
 package sandbox.rest;
 
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -17,20 +21,22 @@ public class BookClient
           .register(JacksonFeature.class)
           .build();
         
+        String URL = loadProperties().getProperty("url");
+        
         //WS text GET - Accept:text/plain
-        WebTarget target = client.target("http://localhost:8080/tomcat-dbcp-rest/").path("books");
+        WebTarget target = client.target(URL).path("books");
         String res = target.request().get().readEntity(String.class);
         System.out.println("A "+ res);
         
         //WS JSON GET 
-        WebTarget target2 = client.target("http://localhost:8080/tomcat-dbcp-rest/").path("books");
+        WebTarget target2 = client.target(URL).path("books");
         String res2 = target2.request(MediaType.APPLICATION_JSON).get().readEntity(String.class);
         System.out.println("B "+ res2);
         
         //WS sending and receiving a String by POST
         {
         	String req = "{\"name\": \"New Book\", \"author\": \"Unknown 1\"}";// a valid json
-        	WebTarget target3 = client.target("http://localhost:8080/tomcat-dbcp-rest/").path("books");
+        	WebTarget target3 = client.target(URL).path("books");
         	String res3 = target3.request().post(Entity.entity(req, MediaType.APPLICATION_JSON), String.class);      
         	System.out.println("C "+ res3);
         }
@@ -42,9 +48,28 @@ public class BookClient
         	requestObj.setName("Stripped");
 
         	System.out.println("D Post: " + requestObj.toString());
-        	WebTarget target4 = client.target("http://localhost:8080/tomcat-dbcp-rest/").path("books");
+        	WebTarget target4 = client.target(URL).path("books");
         	BookObj responseObj = target4.request(MediaType.APPLICATION_JSON).post(Entity.entity(requestObj, MediaType.APPLICATION_JSON)).readEntity(BookObj.class);      
         	System.out.println("D Resp: " + responseObj.toString());
         }
     }
+    
+	public static Properties loadProperties() {
+		Properties props = new Properties();
+		try {
+			InputStream is = BookClientMT.class.getResourceAsStream("/books.properties");
+			if (is != null) {
+				props.load(is);
+				is.close();
+			}
+		}
+		catch(IOException ioe) {
+			System.err.println("IOException in loadProps");
+			for(StackTraceElement ste : ioe.getStackTrace())
+				System.err.println(ste.toString());
+			System.exit(-1);
+		}
+		return props;
+	}
+
 }
